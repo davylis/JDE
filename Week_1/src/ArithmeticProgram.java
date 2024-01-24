351721e0d39243f3d9584604c1a26eb48f6fe2e7
Davylis <liisamuttonen@gmail.com>
2024-01-25 01:39:12 +0200

    Add programs to Java dir

--- Week_1/src/ArithmeticProgram.java
+++ Week_1/src/ArithmeticProgram.java
@@ -1,9 +1,38 @@
+/*
+ * Create a program called ArithmeticProgram that first inputs two 
+ * integers and one decimal number from the user. Then the program 
+ * should do three separate multiplications and show their results as 
+ * shown in the example output.
+ * -The program should accept both comma and dot as decimal part separator.
+ * -The program should not format numbers for printing
+ */
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
-public class ArithmeticProgram {
-
+public class ArithmeticProgram {   
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		DecimalFormat decfor = new DecimalFormat("0.0");
+		// Create a scanner object
+		Scanner scanner = new Scanner(System.in);
+		
+		//input two int and one decimal
+		System.out.print("Enter an integer: ");
+		int number1 = scanner.nextInt();
+		
+		System.out.print("Enter another integer: ");
+		int number2 = scanner.nextInt();
+		
+		System.out.print("Enter an decimal number: ");
+		double decimalNumber = scanner.nextDouble();
+		
+		//Perform 3 multiplications
+		int result1 = number1 * number2;
+		double result2 = number1 * decimalNumber;
+		double result3 = number2 * decimalNumber;
+		
+		System.out.println("\n" + number1 + " * " + number2 + " = " + result1 + "\n " + number1 + " * " + decimalNumber + " = " + decfor.format(result2) + "\n" + number2 + " * " + decimalNumber + " = " + decfor.format(result3));
+		
+		scanner.close();
 	}
 
 }
