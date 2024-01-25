36066a49ea8206deea0d7187f7869b90e0495255
Davylis <liisamuttonen@gmail.com>
2024-01-25 02:52:42 +0200

    Add all programs

--- Week_1/src/ArithmeticProgram.java
+++ Week_1/src/ArithmeticProgram.java
@@ -1,3 +1,4 @@
+
 /*
  * Create a program called ArithmeticProgram that first inputs two 
  * integers and one decimal number from the user. Then the program 
@@ -6,32 +7,36 @@
  * -The program should accept both comma and dot as decimal part separator.
  * -The program should not format numbers for printing
  */
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class ArithmeticProgram {   
+public class ArithmeticProgram {
 	public static void main(String[] args) {
-		DecimalFormat decfor = new DecimalFormat("0.0");
 		// Create a scanner object
 		Scanner scanner = new Scanner(System.in);
-		
-		//input two int and one decimal
+
+		// input two int and one decimal
 		System.out.print("Enter an integer: ");
 		int number1 = scanner.nextInt();
-		
+
 		System.out.print("Enter another integer: ");
 		int number2 = scanner.nextInt();
-		
-		System.out.print("Enter an decimal number: ");
-		double decimalNumber = scanner.nextDouble();
-		
-		//Perform 3 multiplications
+
+		scanner.nextLine();
+
+		System.out.print("Enter a decimal number: ");
+		String decimalNumber = scanner.nextLine();
+		double n = Double.parseDouble(decimalNumber.replace(',', '.'));
+
+		// Perform 3 multiplications
 		int result1 = number1 * number2;
-		double result2 = number1 * decimalNumber;
-		double result3 = number2 * decimalNumber;
-		
-		System.out.println("\n" + number1 + " * " + number2 + " = " + result1 + "\n " + number1 + " * " + decimalNumber + " = " + decfor.format(result2) + "\n" + number2 + " * " + decimalNumber + " = " + decfor.format(result3));
-		
+		double result2 = number1 * n;
+		double result3 = n * n;
+
+		System.out.printf("\n%d * %d = %d\n%d * %.4f = %.3f\n%.4f * %.4f = %f\n",
+                number1, number2, result1,
+                number1, n, result2,
+                n, n, result3);
+
 		scanner.close();
 	}
 
