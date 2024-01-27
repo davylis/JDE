83306951266cdc191b664ab27be50c756137f3d9
Davylis <liisamuttonen@gmail.com>
2024-01-28 01:11:55 +0200

    Add DivisionandRemainder excersise

---
+++ Week_1/src/DivisionAndRemainder.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DivisionAndRemainder {
+
+	public static void main(String[] args) {
+		// Create a scanner object
+		Scanner input = new Scanner(System.in);
+		//input x
+		System.out,print("Enter x: ");
+		int x = readDouble(input);
+		//input y
+		System.out.print("Enter y: ")"Enter x: 
+		int y = readDouble(input);
+		//if-else method
+		if() {
+			
+		}
+		
+
+	}
+	private static double readDouble (Scanner input) {
+		return Double.parseDouble(input.nextLine());
+	}
+}
