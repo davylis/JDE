62ba638d4bebaa2908ee1d3e7b4e5cb61e01d825
Davylis <liisamuttonen@gmail.com>
2024-01-29 09:48:21 +0200

    Add division and remainder program

--- Week_1/src/DivisionAndRemainder.java
+++ Week_1/src/DivisionAndRemainder.java
@@ -5,20 +5,32 @@ public class DivisionAndRemainder {
 	public static void main(String[] args) {
 		// Create a scanner object
 		Scanner input = new Scanner(System.in);
-		//input x
-		System.out,print("Enter x: ");
-		int x = readDouble(input);
-		//input y
-		System.out.print("Enter y: ")"Enter x: 
-		int y = readDouble(input);
-		//if-else method
-		if() {
-			
+		// input x
+		System.out.print("Enter x: ");
+		int x = Integer.parseInt(input.nextLine());
+		// input y
+		System.out.print("Enter y: ");
+		int y = Integer.parseInt(input.nextLine());
+		// if-else method for reaching even or odd for x
+		if (x % 2 == 0) {
+			System.out.println("\n" + x + " is even");
+		} else {
+			System.out.println("\n" + x + " is odd");
 		}
-		
-
+		// if-else method for reaching even or odd for y
+		if (y % 2 == 0) {
+			System.out.println(y + " is even");
+		} else {
+			System.out.println(y + " is odd");
+		}
+		// creating remainder operations
+		int remainder1 = x % y;
+		int remainder2 = y % x;
+		System.out.println("The remainder of " + x + " / " + y + " is " + remainder1);
+		System.out.println("The remainder of " + y + " / " + x + " is " + remainder2);
 	}
-	private static double readDouble (Scanner input) {
+
+	private static double readDouble(Scanner input) {
 		return Double.parseDouble(input.nextLine());
 	}
 }
--- Week_1/src/PopulationProgram.java
+++ Week_1/src/PopulationProgram.java
@@ -1,3 +1,4 @@
+
 /*
  * Create a program called PopulationProgram that first inputs the number of inhabitants of three
 areas from the user. Then the program should calculate the percentage of population in each area.
@@ -13,29 +14,29 @@ public class PopulationProgram {
 		Scanner input = new Scanner(System.in);
 		// Using decimal formatter to get one decimal after a dot
 		DecimalFormat oneDecimal = new DecimalFormat("0.0");
-		//input area A
+		// input area A
 		System.out.print("Enter the population of the area A: ");
 		double areaA = readDouble(input);
-		//input area B
+		// input area B
 		System.out.print("Enter the population of the area B: ");
 		double areaB = readDouble(input);
-		//input area C
+		// input area C
 		System.out.print("Enter the population of the area C: ");
 		double areaC = readDouble(input);
-		//make a calculation
-		double ACalculation =areaA / (areaA + areaB + areaC) * 100;
-		double BCalculation =areaB / (areaA + areaB + areaC) * 100;
-		double CCalculation =areaC / (areaA + areaB + areaC) * 100;
-		//print output
-		System.out.println("\nA: " + oneDecimal.format(ACalculation) + "%" );
-		System.out.println("B: " + oneDecimal.format(BCalculation) + "%" );
-		System.out.println("C: " + oneDecimal.format(CCalculation) + "%" );
-		
+		// make a calculation
+		double ACalculation = areaA / (areaA + areaB + areaC) * 100;
+		double BCalculation = areaB / (areaA + areaB + areaC) * 100;
+		double CCalculation = areaC / (areaA + areaB + areaC) * 100;
+		// print output
+		System.out.println("\nA: " + oneDecimal.format(ACalculation) + " %");
+		System.out.println("B: " + oneDecimal.format(BCalculation) + " %");
+		System.out.println("C: " + oneDecimal.format(CCalculation) + " %");
+		// close scanner
 		input.close();
-		
-
+		// method to obtain input
 	}
-		private static double readDouble(Scanner input){
-			return Double.parseDouble(input.nextLine());
-		}
+
+	private static double readDouble(Scanner input) {
+		return Double.parseDouble(input.nextLine());
+	}
 }
