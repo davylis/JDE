4c95f7061cb7e856f4dd682273f0628863a64b8f
Davylis <liisamuttonen@gmail.com>
2024-02-02 12:20:31 +0200

    Add StringTrio program

--- Week_3/src/StringTrio.java
+++ Week_3/src/StringTrio.java
@@ -1,9 +1,33 @@
+/*
+ * Create a program called StringTrio that first inputs three strings from the user. Then the program
+compares the strings and displays one of the following:
+• The strings are equal in case-sensitive comparison
+• The strings are equal in case-insensitive comparison
+• The strings are not equal
+ */
+import java.util.Scanner;
 
 public class StringTrio {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//create scanner
+		Scanner input = new Scanner(System.in);
+		//input first string
+		System.out.print("Enter first string: ");
+		String first = input.nextLine();
+		//input second string
+		System.out.print("Enter first string: ");
+		String second = input.nextLine();
+		//input third string
+		System.out.print("Enter first string: ");
+		String third = input.nextLine();
+		//compare strings in case-sensitive and case insensitive
+		if (first.equals(second) && first.equals(third)) {
+			System.out.println("The strings are equal in case-sensitive comparison");
+		} else if(first.equalsIgnoreCase(second) && first.equalsIgnoreCase(third)){
+			System.out.println("The strings are equal in case-insensitive comparison");
+		} else {
+			System.out.println("The strings are not equal");
+		}
 	}
-
 }
