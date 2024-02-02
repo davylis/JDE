d2fd4e5e811d48563e69f84d4fb1b219593a2d94
Davylis <liisamuttonen@gmail.com>
2024-02-02 11:28:07 +0200

    Modify PositiveNumbers program

--- Week_3/src/PositiveNumbers.java
+++ Week_3/src/PositiveNumbers.java
@@ -1,5 +1,6 @@
 /*
- * Create a program called PositiveNumbers that first inputs integers from the user until the user
+ * Create a program called PositiveNumbers 
+that first inputs integers from the user until the user
 decides to quit by entering a zero. Then the program should compute and print the average of the
 entered positive integers with one decimal. If the only entered value is a zero, the program should
 print "No positive values"
@@ -16,33 +17,37 @@ public class PositiveNumbers {
 		int count = 0;
 		//input integer
 		System.out.print("Enter first integer: ");
-		//int numbers = Integer.parseInt(input.nextLine());
 		//make while-loop for integer input
 		while(true) {
+			//try-catch to catch errors
 			try {
 				int number = Integer.parseInt(input.nextLine());
-				
+				//if number is zero, end loop
 				if(number == 0) {
 					break;
 				}
+				//if number is above zero, count it in variables
 				if(number > 0){
 					total += number;
 					count ++;
 					
 					System.out.print("Enter next integer: ");
 				}	
+			//if number is under zero or character, print error
 			} catch (NumberFormatException e) {
 				System.out.println("Invalid input. Please enter an integer. ");
 				System.out.print("Enter next integer: ");
 			}
 		}
-		
+		//if input number is only 0
 		if (count == 0) {
 			System.out.println("No positive values.");
 		} else {
+			//calculate average
 			double average = (double) total / count;
 			System.out.printf("The average of the positive values is %.1f%n", average);
 		}
+		//close scanner
 		input.close();
 	}
 }
