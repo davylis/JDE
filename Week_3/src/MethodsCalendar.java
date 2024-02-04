60de4de92566f3e6f0dbdc63ddd3210e4ed8725e
Davylis <liisamuttonen@gmail.com>
2024-02-04 23:46:40 +0200

    Modify calender program

--- Week_3/src/MethodsCalendar.java
+++ Week_3/src/MethodsCalendar.java
@@ -9,29 +9,34 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class MethodsCalendar {
-	
-	public static void printCalendar (int month, int year) {
-		String[] months = {"Tammikuu", "Helmikuu", "Maaliskuu", "Huhtikuu", "Toukokuu",
-				"Kesäkuu", "Heinäkuu", "Elokuu", "Syyskuu", "Lokakuu", "Marraskuu", "Joulukuu"};
-		
-		String monthString = months[month-1];
-		
-		System.out.println(" > " + monthString + " " + year + " < ");
-		System.out.println("Ma Ti Ke To Pe La Su ");
-		
+	//declares a method named "printCalendar" that takes two parameters
+	public static void printCalendar(int month, int year) {
+		//Initialize an array
+		String[] months = { "Tammikuu", "Helmikuu", "Maaliskuu", "Huhtikuu", "Toukokuu", "Kesäkuu", "Heinäkuu",
+				"Elokuu", "Syyskuu", "Lokakuu", "Marraskuu", "Joulukuu" };
+		//assign the month name corresponding to given month number
+		String monthString = months[month - 1];
+
+		System.out.println(" > " + monthString + " " + year + " <");
+		System.out.println(" Ma Ti Ke To Pe La Su");
+		//Use LocalDate to get first day of the specified month and calculate the total
+		//days in the month and the day of the week for the first day
+		//create a LocalDate object named myDate
 		LocalDate myDate = LocalDate.of(year, month, 1);
 		int daysInMonth = myDate.lengthOfMonth();
 		int daysOfWeek = myDate.getDayOfWeek().getValue();
-		
+		//Prints empty spaces corresponding to the day of the week for the first day
 		int dayCounter = 1;
-		
-		for(int = 1; i < daysOfWeek; i++) {
-			System.out.print("    ");
+
+		for (int i = 1; i < daysOfWeek; i++) {
+			System.out.print("   ");
 		}
-		for(int day = 1; day <= daysInMonth; day++) {
+		//Print days of the month with proper formatting
+		//Checks for the end of the week or end of the month to move to the next line
+		for (int day = 1; day <= daysInMonth; day++) {
 			System.out.printf("%3d", day);
-			
-			if((day + daysOfWeek - 1) % 7 == 0 || day == daysInMonth) {
+			//
+			if ((day + daysOfWeek - 1) % 7 == 0 || day == daysInMonth) {
 				System.out.println();
 			}
 		}
@@ -39,15 +44,16 @@ public class MethodsCalendar {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.print("Enter year: ");
 		int year = Integer.parseInt(input.nextLine());
-		
+
 		System.out.print("Enter month: ");
 		int month = Integer.parseInt(input.nextLine());
-		
+		System.out.println();
+
 		printCalendar(month, year);
-		
+
 		input.close();
 	}
 
