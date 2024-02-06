1ad8f7f9d35c385d6b9dd0846cdbfa17a111350e
Davylis <liisamuttonen@gmail.com>
2024-02-06 11:54:21 +0200

    Add Array Iteration program

--- Week_3/src/ArraysIteration.java
+++ Week_3/src/ArraysIteration.java
@@ -1,15 +1,29 @@
-
+/*
+ * Create a program called ArraysIteration that first inputs five 
+ * integers from the user and saves them in an array. Then the program
+ *  should print the integers in descending order on a single line.
+ */
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Collections;
 
 public class ArraysIteration {
 
 	public static void main(String[] args) {
-		System.out.println(Enter an integer: );
-		int 
+		Scanner input = new Scanner(System.in);
+		Integer[] array = new Integer[5];
 		
-		int array[] = {17, 1, 2, 0, 2, 3};
-		for(int i = 0; i < array.length; i++) {
-		System.out.print(array[i]+" ");
+		for(int i = 0; i < 5; i++) {
+			System.out.print("Enter an integer: ");
+			array[i] = Integer.parseInt(input.nextLine());
+		}
+		
+		Arrays.sort(array, Collections.reverseOrder());
+		System.out.println();
+		
+		for(int j = 0; j < array.length; j++) {
+			System.out.print(array[j] + " ");
 		}
 	}
 }
+
