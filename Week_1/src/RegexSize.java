6ef7ef496e6b0e4b13c6349b2edaf0f0afe0df42
Davylis <liisamuttonen@gmail.com>
2024-02-12 18:29:02 +0200

    Add Regexsize program

--- Week_1/src/RegexSize.java
+++ Week_1/src/RegexSize.java
@@ -1,8 +1,24 @@
+/*
+ * Create a program called RegexSize that first inputs size code (XXS, XS, S, M, L, XL, XXL) from the
+user. The program should print either "Size ok" or "Invalid size". Please validate the inputted data
+with a single regular expression.
+ */
+
+import java.util.Scanner;
 
 public class RegexSize {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter size: ");
+		String size = input.nextLine();
+
+		if(size.matches("XX[SL]|X[SM]|L|M")) {
+			System.out.println("Size ok");
+		}else {
+			System.out.println("Invalid size");
+		}
 
 	}
 
